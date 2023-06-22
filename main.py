ee6a40eaa60bb96917a057201499ec43fe7919fd
88018 <mhrm201@gmail.com>
2023-06-23 02:25:04 +0600

    print multiplication of even numbers

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-# for i in range(0, 10):
-#     print(i, end=', ') if i % 2 == 1 else None
+# INPUT
 
-# print(', '.join([str(x) if x % 2 == 1 else m for x in range(10)]))
-odd = list(filter(lambda x: x % 2 == 0, range(10)))
-
-print(','.join(map(str, odd)))
+ar = list(range(2, 10, 2))
+print(ar)
 
 
+# YOUR CODE HERE
+multi = 1
+
+for item in ar:
+    multi *= item
+
+print(multi)
