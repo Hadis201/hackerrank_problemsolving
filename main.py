78092ecb35e769877d68f1f58110135fa4fb8117
88018 <mhrm201@gmail.com>
2023-06-23 02:00:39 +0600

    print even numbers

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #     print(i, end=', ') if i % 2 == 1 else None
 
 # print(', '.join([str(x) if x % 2 == 1 else m for x in range(10)]))
-odd = list(filter(lambda x: x % 2 == 1, range(10)))
+odd = list(filter(lambda x: x % 2 == 0, range(10)))
 
 print(','.join(map(str, odd)))
 
