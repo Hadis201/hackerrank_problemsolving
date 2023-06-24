5f432ddd04bac63ff6a25fc1bad2c4234bea4236
88018 <mhrm201@gmail.com>
2023-06-24 12:25:58 +0600

    anis vai tutorial 1st part

---
+++ anis vai tutorial.py
@@ -0,0 +1,10 @@
+#print("hadis \" 01814603194  ")
+
+name = "Hadis"
+age = 21
+result = 3.78
+print(" Hi my n ame is "+name)
+print("I am ",+age,". I do hope that I will acquare cgpa ", +result)
+print("I am new in the engineering area.")
+print("As it is a vast area of prosperity, I am having a vast pleasure ")
+print("As it is a vast area of prosperity, I am having a vast pleasure ") 
---
+++ area related anis vai.py
@@ -0,0 +1,4 @@
+radius = float(input("enter the value of radius of the circle: ").strip())
+
+area= 3.1416*radius**2
+print("area of the circle", area)
